9b5d73f76893105f80e8ae29edebfcc241ecdf87
rck5005 <rck5005@gmail.com>
2024-08-13 17:54:49 -0400

    created all apps for django

---
+++ cookie_cutter_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CookieCutterAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cookie_cutter_app'
---
+++ user_cookie_creation_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UserCookieCreationAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'user_cookie_creation_app'
